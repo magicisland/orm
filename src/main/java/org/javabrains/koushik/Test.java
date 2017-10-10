669c4d95b5e056d7924e6bbe9597452552ba9bcd
magicisland <marcelojavierrodriguez@gmail.com>
2017-10-10 06:53:29 -0300

    Test que sirve

---
+++ src/main/java/org/javabrains/koushik/Test.java
@@ -0,0 +1,15 @@
+package org.javabrains.koushik;
+
+import org.javabrains.koushik.dto.UserDetails;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		UserDetails user = new UserDetails();
+		user.setUserId(1);
+		user.setUserName("Marcelo");
+	}
+
+}
