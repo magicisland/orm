da79651acf7e96677a7ddae72675d72ae242c0e7
magicisland <marcelojavierrodriguez@gmail.com>
2017-10-10 06:22:14 -0300

    just the getters and setters methods

--- src/main/java/org/javabrains/koushik/dto/UserDetails.java
+++ src/main/java/org/javabrains/koushik/dto/UserDetails.java
@@ -1,5 +1,32 @@
 package org.javabrains.koushik.dto;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table (name="USER_DETAILS")
 public class UserDetails {
 
+	protected int userId;
+	protected String userName;
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	@Override
+	public String toString() {
+		return "UserDetails [userId=" + userId + ", userName=" + userName + "]";
+	}
+	
+	public UserDetails(){
+		
+	}
 }
